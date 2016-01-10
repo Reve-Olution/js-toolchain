bf08a9074e7ccfc486971209cd3e1617d8c4e956
Arnaud Geiser <arnaudgeiser@gmail.com>
2016-01-10 15:17:18 +0100

    Added demoGulp directory

---
+++ demoGulp/app/scripts/scripts.js
@@ -0,0 +1,42 @@
+    var demoGruntApp = angular.module('demoGruntApp', ['ngRoute']);
+
+    // configure our routes
+    demoGruntApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/home', {
+                templateUrl : 'views/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/about', {            	
+                templateUrl : 'views/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'views/contact.html',
+                controller  : 'contactController'
+            })
+
+			.otherwise({
+        		redirectTo: '/home'
+      		});
+    });
+
+    // create the controller and inject Angular's $scope
+    demoGruntApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Bienvenue sur notre site !!';
+    });
+
+    demoGruntApp.controller('aboutController', function($scope) {
+        $scope.message = 'A propos de nous !';
+    });
+
+    demoGruntApp.controller('contactController', function($scope) {
+        $scope.message = 'Envoyez nous un petit message ! :)';
+    });
